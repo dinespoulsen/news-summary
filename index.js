1a351453d5c3a78199e6a68435ecaaf7ab7ac2e6
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-07 17:12:41 +0000

    news controller can update view

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
 var newsController = new NewsController();
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body&from-date=2017-01-01&q=javascript", true)
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    this.myText = JSON.parse(xhr.responseText);
-    var link = document.getElementById("links");
-    console.log(this.myText.response.results.length);
-    for (i = 0; i < this.myText.response.results.length; i += 1){
-      console.log(this.myText.response.results[i].webTitle)
-      link.innerHTML += "<li><a href=\"#\">" + this.myText.response.results[i].webTitle + "</li></a>";
-    };
-
-    console.log(this.myText);
-  }
-};
-
-xhr.send();
+newsController.requestNews("javascript", newsController.updateViewList.bind(newsController));
--- newsController.js
+++ newsController.js
@@ -4,26 +4,29 @@
 
   };
 
-  NewsController.prototype.requestNews = function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body&from-date=2017-01-01&q=javascript", true)
-    xhr.onreadystatechange = function() {
+  NewsController.prototype.requestNews = function(search, updateViewList){
+    this.xhr = new XMLHttpRequest();
+    this.xhr.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body&from-date=2017-01-01&q=" + search, true)
+    this.xhr.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        this.myText = JSON.parse(xhr.responseText);
-        var link = document.getElementById("links");
-        console.log(this.myText.response.results.length);
-        for (i = 0; i < this.myText.response.results.length; i += 1){
-          console.log(this.myText.response.results[i].webTitle)
-          link.innerHTML += "<li><a href=\"#\">" + this.myText.response.results[i].webTitle + "</li></a>";
-        };
-
+        this.myText = JSON.parse(this.responseText);
         console.log(this.myText);
+        updateViewList();
       }
     };
-
-    xhr.send();
-
+    this.xhr.send();
   };
+
+  NewsController.prototype.updateViewList = function(){
+    console.log("test")
+    var link = document.getElementById("links");
+    console.log(this);
+    console.log(this.xhr.myText.response.results.length);
+    for (i = 0; i < this.xhr.myText.response.results.length; i += 1){
+      console.log(this.xhr.myText.response.results[i].webTitle)
+      link.innerHTML += "<li><a href=\"#\">" + this.xhr.myText.response.results[i].webTitle + "</li></a>";
+    };
+  }
 
   exports.NewsController = NewsController;
 
